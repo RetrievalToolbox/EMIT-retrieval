dbe7ca8b2f22e75516bc79c89ca88ca93e822e88
PeterSomkuti <peter.somkuti@gmail.com>
2025-12-03 10:35:44 -0500

    Updates to setup.sh

--- setup.sh
+++ setup.sh
@@ -6,18 +6,26 @@
 # Currently, we see the example working well in this version
 julia_version=1.11.7
 
-echo "### Installing Julia via juliaup"
-
-juliaup -V
+juliaup -V # Check if JuliaUp is available at all
 if [ $? -ne 0 ]; then
-    echo "JuliaUp not found - installing"
+    echo "### JuliaUp not found - installing"
     curl -fsSL https://install.julialang.org | sh -s -- --default-channel ${julia_version}
+    echo "### Julia was installed. Please re-start or re-source your shell!"
+    exit
+fi
+
+
+# Check if the correct version is available
+
+if `juliaup status | grep -q ${julia_version}`; then
+    echo "### Correct Julia version ${julia_version} found!"
 else
-    echo "JuliaUp found - installing ${julia_version}."
+    echo "### JuliaUp found, but need a specific version - installing ${julia_version}."
     juliaup add ${julia_version}
 fi
 
 # Install Julia packages
+echo "### Installing required Julia packages."
 julia +${julia_version} --project="./" -e 'using Pkg; Pkg.instantiate();'
 
 # Install gdown to get Google Drive-stored data
